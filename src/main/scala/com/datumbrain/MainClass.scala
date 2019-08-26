7fa9b20326d19b7b836cf02b57d46f53bf03b1d3
saadali5997 <saadali5997@gmail.com>
2019-08-26 18:13:55 +0500

    Initial commit.

--- src/main/scala/com/datumbrain/MainClass.scala
+++ src/main/scala/com/datumbrain/MainClass.scala
@@ -1,12 +1,59 @@
 package com.datumbrain
 
+import java.io.FileInputStream
+import java.nio.file.Paths
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.storage.{BlobId, StorageOptions}
+
 /**
  * @author ${user.name}
  */
 object MainClass {
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
+
+  private val credentialFileAddress: String = "/home/saad/learning/google-cloud/my-first-project-995398db50b9.json"
+  private val credentials = GoogleCredentials.fromStream(new FileInputStream(s"$credentialFileAddress"))
+  GoogleCredentials.
+  private val storage = StorageOptions.newBuilder.setCredentials(credentials).setProjectId("adroit-minutia-250910").build.getService
+
+  private val bucket = storage.get("test-bucket-fahad")
+
+  def downloadFile() = {
+    val blobs = bucket.list().iterateAll()
+    val path = Paths.get("/home/saad/learning/google-cloud/saad ali.pdf")
+    blobs.forEach { blob =>
+      if (blob.getBlobId.getName.endsWith("saad ali.pdf")) {
+        val blob = storage.get(BlobId.of("test-bucket-fahad", "saad ali.pdf"))
+        blob.downloadTo(path)
+      }
+    }
+
+  }
+
+  def main(args: Array[String]) {
+    printBucketContent()
+    downloadFile()
+//    uploadFile("/home/saad/Desktop/Saad Ali.pdf")
+//    printBucketContent()
+  }
+
+  def printBucketContent(): Unit = {
+    println("******************************************************")
+    println("**************** PRINTING BUCKET CONTENT *************")
+    println("******************************************************")
+
+    println(s"Bucket generated Id => ${bucket.getGeneratedId}")
+
+    val blobs = bucket.list()
+    blobs.iterateAll().forEach { blob =>
+      println(blob.getBlobId.getName)
+    }
+  }
+
+  def uploadFile(filePath: String) = {
+    val fileToUpload = new FileInputStream(filePath)
+    val fileName = filePath.split("/").last.toLowerCase
+    val blob = bucket.create(fileName, fileToUpload)
   }
 
 }
